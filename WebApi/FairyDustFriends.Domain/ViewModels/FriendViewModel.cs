546ffe6edd484ffac6538711791444ef33a89de6
vagnerjsmello <vagnerjsmello@gmail.com>
2018-09-03 10:20:41 -0300

    Add DataAnnotations [Required]

--- WebApi/FairyDustFriends.Domain/ViewModels/FriendViewModel.cs
+++ WebApi/FairyDustFriends.Domain/ViewModels/FriendViewModel.cs
@@ -1,15 +1,21 @@
 ﻿
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FairyDustFriends.Domain.ViewModels
 {
     public class FriendViewModel
-    {
+    {        
         public String Id { get; set; }
+        [Required]
         public String FirstName { get; set; }
+        [Required]
         public String LastName { get; set; }
+        [Required]
         public String Email { get; set; }
+        [Required]
         public String Phone { get; set; }
+        [Required]
         public String Birthday { get; set; }
     }
 }
